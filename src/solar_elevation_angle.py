a19761d250e90c6641824313575f87e74359b7d3
OliverLipczak <s240240@student.dtu.dk>
2025-04-08 13:37:51 +0200

    Update solar_elevation_angle.py
    udregner i radianer

--- src/solar_elevation_angle.py
+++ src/solar_elevation_angle.py
@@ -1,4 +1,5 @@
 #funktionen for solar elevation 
+import numpy as np
 def solar_elevation_angle(theta):
-    alpha = 90 -theta
-    return alpha #bruger formlen til at udregne alpha
+    alpha = np.deg2rad((90 - theta))
+    return alpha #bruger formlen til at udregne alpha i radianer
